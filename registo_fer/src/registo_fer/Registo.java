750ff427c8c80aa4bbe702a20d4172445f162b7a
David Patricio Escamilla Marquez <46275040+MagoPato@users.noreply.github.com>
2021-09-06 01:19:50 -0500

    ACTIVIDAD 2
    CREACION DEL OBJETO

---
+++ registo_fer/src/registo_fer/Registo.java
@@ -0,0 +1,61 @@
+package registo_fer;
+
+public class Registo {
+
+    private String Marca;
+    private String modelo;
+    private double talla;
+    private int serie;
+
+    public Registo() {
+        this.Marca = " no tiene ";
+        this.modelo = " no tiene ";
+        this.talla = 0;
+        this.serie = 0;
+    }
+
+    public Registo(int serie) {
+        this.serie = serie;
+    }
+    
+     public Registo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getTalla() {
+        return talla;
+    }
+
+    public void setTalla(double talla) {
+        this.talla = talla;
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
+
+    @Override
+    public String toString() {
+        return "Marca es "+getMarca()+", Modelo es "+getModelo()+", Talla es "+getTalla()+", Serie es "+getSerie();
+    }
+}
---
+++ registo_fer/src/registo_fer/Registo_fer.java
@@ -0,0 +1,26 @@
+package registo_fer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Registo_fer {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        Registo Registo[] = new Registo[10];
+
+        for (int i = 0; i < 10; i++) {
+            Registo[i] = new Registo();
+            System.out.println("DIME TU MARCA");
+            Registo[i].setMarca(entrada.next());
+            System.out.println("DIME TU TALLA");
+            Registo[i].setTalla(entrada.nextDouble());
+            System.out.println("DIME EL MODELO");
+            Registo[i].setModelo(entrada.next());
+            System.out.println("DIME EL NUM DE SERIE");
+            Registo[i].setSerie(entrada.nextInt());
+        }
+            System.out.println(Arrays.toString(Registo));
+    }
+}
